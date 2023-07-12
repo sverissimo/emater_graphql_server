448c1a6ab7b05da4c85b926e8f4af4e5f3152e6b
sverissimo <sandroverissimo@live.com>
2023-07-11 22:01:32 -0300

    Esboço inicial de db e entidades utilizando prisma e graphql.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,30 +1,61 @@
-// prisma/seed.ts
 import { PrismaClient } from "@prisma/client";
 import proprietarios from "../data/ger_pessoa.json" assert { type: "json" };
 import propriedades from "../data/public_pl_propriedade_subset.json" assert { type: "json" };
+import produtorPropriedades from "../data/linkedTableInput.json" assert { type: "json" };
+import perfis from "../data/public_at_prf_see_export_2023-07-07_002028.json" assert { type: "json" };
+import dadosProducao from "../data/public_at_prf_see_dados_producao_export_2023-07-07_002104.json" assert { type: "json" };
+import propriedadeVisitaLink from "../data/public_at_prf_see_propriedade_export_2023-07-07_002120.json" assert { type: "json" };
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.proprietario.createMany({
-    data: proprietarios,
-  });
-
   await prisma.propriedade.createMany({
     data: propriedades.map((item) => ({
       ...item,
+      id_pl_propriedade: Number(item.id_pl_propriedade),
+      area_total: Number(item.area_total),
       dt_update_record: new Date(item.dt_update_record),
       area_condominio: Boolean(item.area_condominio),
       ativo: Boolean(item.ativo),
-      sn_ctd: Boolean(item.sn_ctd),
+      distancia_sede: Number(item.distancia_sede),
     })),
   });
 
-  /* // Insert visitas
-  await prisma.visita.createMany({
-    data: data.visitas,
+  await prisma.atividade.createMany({
+    data: propriedadeVisitaLink.map((p) => {
+      const result = {
+        ...p,
+        id_propriedade: parseInt(p.id_propriedade),
+      };
+      return result;
+    }),
   });
 
+  await prisma.dadosProducao.createMany({
+    data: dadosProducao,
+  });
+  await prisma.perfil.createMany({
+    data: perfis.map((p) => ({
+      ...p,
+      data_preenchimento: new Date(p.data_preenchimento),
+      data_atualizacao: new Date(p.data_atualizacao)
+        .toISOString()
+        .slice(0, "yyyy-mm-dd".length),
+    })),
+  });
+
+  await prisma.produtor.createMany({
+    data: proprietarios.map((item) => ({
+      ...item,
+      id_pessoa_demeter: BigInt(item.id_pessoa_demeter), // convert to BigInt
+      dt_nascimento: new Date(item.dt_nascimento),
+      dt_update_record: new Date(item.dt_update_record),
+    })),
+  });
+
+  await prisma.produtorPropriedades.createMany({ data: produtorPropriedades });
+
+  /*
   // Insert pictureFiles
   await prisma.pictureFile.createMany({
     data: data.pictureFiles,
@@ -45,17 +76,3 @@ main()
   .finally(async () => {
     await prisma.$disconnect();
   });
-
-/*   const sanitizeData = (data: any[]) => {
-    const proprietarios = data.map((proprietario) => {
-      const sanitizedProprietario = { ...proprietario };
-      for (const key in sanitizedProprietario) {
-        if (sanitizedProprietario[key] === null) {
-          delete sanitizedProprietario[key];
-        }
-      }
-      return sanitizedProprietario;
-    });
-    return proprietarios;
-  };
-   */
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,16 +1,80 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-const proprietarios = prisma.proprietario.findMany();
-const propriedades = prisma.propriedade.findMany();
+const produtores = await prisma.produtor.findMany({
+  include: {
+    produtor_propriedades: true,
+  },
+});
+
+const propriedades = await prisma.propriedade.findMany({
+  include: {
+    produtor_propriedade: {
+      include: { produtor: true },
+    },
+  },
+});
+
+const produtoresFull = await prisma.produtorPropriedades.findMany({
+  include: {
+    produtor: true,
+    propriedade: true,
+  },
+});
+
+const perfilData = await prisma.perfil.findMany({
+  include: {
+    atividade: {
+      include: {
+        propriedade: {
+          include: {
+            produtor_propriedade: { include: { produtor: true } },
+          },
+        },
+      },
+    },
+
+    dados_producao: true,
+  },
+  /* where: {
+    atividade: { is: { propriedade: { is: { id_pl_propriedade: 53512 } } } },
+  }, */
+});
 
 export const resolvers = {
   Query: {
+    produtores: () => produtores,
     propriedades: () => propriedades,
-    proprietarios: () => proprietarios,
+    produtoresFull: () => produtoresFull,
+    perfil: () => perfilData,
+  },
+  Atividade: {
+    id_propriedade: (p: any) => parseInt(p.id_propriedade),
+  },
+  Perfil: {
+    data_preenchimento: (p: any) =>
+      p.data_preenchimento.toISOString().slice(0, "yyyy-mm-dd".length),
+    data_atualizacao: (p: any) =>
+      p.data_atualizacao.toISOString().slice(0, "yyyy-mm-dd".length),
+  },
+  Produtor: {
+    id_pessoa_demeter: (p: any) => parseInt(p.id_pessoa_demeter),
+    propriedades: (p: any) => {
+      const propriedadeIds = produtoresFull
+        .filter((t) => t.produtor_id === p.id_pessoa_demeter)
+        .map((p) => p.propriedade_id);
+
+      return prisma.propriedade.findMany({
+        where: { id_pl_propriedade: { in: propriedadeIds } },
+      });
+    },
+  },
+
+  Propriedade: {
+    id_pl_propriedade: (p: any) => parseInt(p.id_pl_propriedade),
+  },
+  ProdutorPropriedades: {
+    produtor_id: (p: any) => parseInt(p.id_proprietario),
+    propriedade_id: (p: any) => parseInt(p.id_propriedade),
   },
 };
-
-/* import propriedades from "../data/public_pl_propriedade_export_2023-07-06_231354.json" assert { type: "json" };
-import proprietarios from "../data/ger_pessoa.json" assert { type: "json" };
- */
